cbce5300843f231c33a8f278e11eb2e7a6ec3a86
Md Zayed <zayed.fakruddin@gmail.com>
2025-03-31 19:22:47 +0530

    Initial commit with frontend and backend

---
+++ enterprise-expense-management/src/main/java/com/expense/management/EnterpriseExpenseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.expense.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseExpenseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseExpenseManagementApplication.class, args);
+	}
+
+}
---
+++ enterprise-expense-management/src/test/java/com/expense/management/EnterpriseExpenseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expense.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseExpenseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
